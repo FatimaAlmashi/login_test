a626b552de05bfef41fc04492f8c9747a83f7df8
Fatima Almashi <f.almashi@windowslive.com>
2021-02-16 10:51:42 +0300

    2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouterModule, RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
+import { HomePage } from './home/home.page';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ListComponent,
+    HomePage
+  ],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'list',
+        component: ListComponent
+      },
+      {
+        path: '',
+        component: HomePage
+      }
+    ]),
+    IonicModule.forRoot(), 
+    AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-login',
+  // moduleId: module.id,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+
+  // LoginForm: FormGroup;
+  // username: AbstractControl;
+  // password: AbstractControl;
+  // // public response: ResponseModel;
+  // // public responseNT: ResponseModel;
+  // // NT: NotificationModel;
+  // service: any;
+  // EMP_TYPE: string;
+
+  constructor(private router: Router) { 
+    // this.LoginForm = fb.group({
+    //   username: ['', Validators.compose([Validators.required, Validators.minLength(1),
+    //   ])],
+    //   password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+    // });
+
+    // this.username = this.LoginForm.controls.username;
+    // this.password = this.LoginForm.controls.password;
+    // // this.service = _service;
+  }
+
+  ngOnInit() {}
+
+  LoginUser(event){
+    event.preventDefault()
+    console.log(event)
+    this.router.navigate(['list']);
+
+    // console.log('username : '+this.username)
+    // console.log('password : '+this.password)
+
+
+    // event = event as HTMLElement
+    // console.log((event.target as Element).id)
+
+    // const target = event.target[0]
+
+    // const username = target.getElementById('username')
+    // const password = target.getElementById('password')
+    
+    // // console.log(username, password)
+  }
+
+}
